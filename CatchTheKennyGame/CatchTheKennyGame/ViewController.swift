cac463c09762e566a9fd4e5409519d29d39b4efd
tahaturan <tahaaturann16@gmail.com>
2023-04-26 11:14:22 +0300

    Score arttirma islemi yapildi

--- CatchTheKennyGame/CatchTheKennyGame/ViewController.swift
+++ CatchTheKennyGame/CatchTheKennyGame/ViewController.swift
@@ -24,11 +24,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var kenny8: UIImageView!
     @IBOutlet weak var kenny9: UIImageView!
     
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scoreLabel.text = "Score: \(score)"
+        
+        kenny1.isUserInteractionEnabled = true
+        kenny2.isUserInteractionEnabled = true
+        kenny3.isUserInteractionEnabled = true
+        kenny4.isUserInteractionEnabled = true
+        kenny5.isUserInteractionEnabled = true
+        kenny6.isUserInteractionEnabled = true
+        kenny7.isUserInteractionEnabled = true
+        kenny8.isUserInteractionEnabled = true
+        kenny9.isUserInteractionEnabled = true
+
+        
+        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(increaseScroe))
+        
+        kenny1.addGestureRecognizer(recognizer1)
+        kenny2.addGestureRecognizer(recognizer2)
+        kenny3.addGestureRecognizer(recognizer3)
+        kenny4.addGestureRecognizer(recognizer4)
+        kenny5.addGestureRecognizer(recognizer5)
+        kenny6.addGestureRecognizer(recognizer6)
+        kenny7.addGestureRecognizer(recognizer7)
+        kenny8.addGestureRecognizer(recognizer8)
+        kenny9.addGestureRecognizer(recognizer9)
+
+        
+        
     }
 
+    @objc func increaseScroe()  {
+        score += 1
+        
+        scoreLabel.text = "Score: \(score)"
+    }
 
 }
 
