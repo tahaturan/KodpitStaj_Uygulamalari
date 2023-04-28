808a7e2a1242946d9a773e0587b6fe63b2eb789d
tahaturan <tahaaturann16@gmail.com>
2023-04-28 15:30:51 +0300

    Silme islemi

--- ArtBookProject/ArtBookProject/DetailsVC.swift
+++ ArtBookProject/ArtBookProject/DetailsVC.swift
@@ -18,9 +18,59 @@ class DetailsVC: UIViewController,UIImagePickerControllerDelegate, UINavigationC
     
     @IBOutlet weak var yearTextField: UITextField!
     
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if chosenPainting != ""{
+            //Core Data
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            
+            let idString = chosenPaintingId?.uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            
+            do {
+               let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject]{
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameTextField.text = name
+                        }
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistTextField.text = artist
+                        }
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearTextField.text = String(year)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                    }
+                    
+                }
+                
+            } catch  {
+                print(error)
+            }
+            
+            
+            
+            fetchRequest.returnsObjectsAsFaults = false
+            
+        }
         
 
         
@@ -82,6 +132,10 @@ class DetailsVC: UIViewController,UIImagePickerControllerDelegate, UINavigationC
         }catch {
             print("Error")
         }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        
+        self.navigationController?.popViewController(animated: true)
         
     }
 
--- ArtBookProject/ArtBookProject/ViewController.swift
+++ ArtBookProject/ArtBookProject/ViewController.swift
@@ -6,22 +6,149 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
-    
     @IBOutlet weak var tableView: UITableView!
+    
+    var nameArray:[String] = [String]()
+    var idArray:[UUID] = [UUID]()
+    var selectedPaintig = ""
+    var selectedPaintigId:UUID?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+      getData()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newData"), object: nil)
+    }
+  
+    
     @IBAction func addButtonClicked(_ sender: Any) {
+        selectedPaintig = ""
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
+    
+   @objc func getData()  {
+       
+       nameArray.removeAll()
+       idArray.removeAll()
+       
+       
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results =  try context.fetch(fetchRequest)
+            
+            if results.count > 0{
+                
+                for result in results as! [NSManagedObject]{
+                    if let name =  result.value(forKey: "name") as? String{
+                        self.nameArray.append(name)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID{
+                        self.idArray.append(id)
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+            
+        } catch  {
+            print(error)
+        }
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationVC = segue.destination as! DetailsVC
+            
+            destinationVC.chosenPainting = selectedPaintig
+            destinationVC.chosenPaintingId = selectedPaintigId
+        }
+    }
+    
     
 
 }
 
+
+extension ViewController:UITableViewDelegate , UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        selectedPaintig = nameArray[indexPath.row]
+        selectedPaintigId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+  
+    func tableView(_ tableView: UITableView , commit editingStyle: UITableViewCell.EditingStyle , forRowAt indexPath:IndexPath){
+        if editingStyle == .delete{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = idArray[indexPath.row].uuidString
+            
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0{
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == idArray[indexPath.row]{
+                                context.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                
+                                do {
+                                    try context.save()
+                                } catch  {
+                                    print(error)
+                                }
+                                
+                                break
+                            }
+                        }
+                    }
+                }
+            } catch  {
+                print(error)
+            }
+        }
+    }
+    
+}
