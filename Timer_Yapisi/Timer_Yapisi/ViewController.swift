fd78cb387a61a3a212aae28cb11c1086d7ae9bd0
tahaturan <tahaaturann16@gmail.com>
2023-05-02 11:01:39 +0300

    Timer Yapisi Sayac uygulamasi

--- Timer_Yapisi/Timer_Yapisi/ViewController.swift
+++ Timer_Yapisi/Timer_Yapisi/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ciktiLabel: UILabel!
+    
+    var sayici:Timer?
+    var sayac:Int = 1
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func baslaButton(_ sender: Any) {
+        sayici = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ileriSay), userInfo: nil, repeats: true)
+    }
+    
+    @objc func ileriSay()  {
+        ciktiLabel.text = String(sayac)
+        sayac += 1
+        
+        if sayac > 6 {
+            sayici?.invalidate()
+            ciktiLabel.text = "Sure Bitti"
+            sayac = 1
+        }
+    }
+    
 }
 
