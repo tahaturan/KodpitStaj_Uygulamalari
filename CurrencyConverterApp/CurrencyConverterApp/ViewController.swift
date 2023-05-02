3216bdbb032840731a0f39664100d4d167cf425b
tahaturan <tahaaturann16@gmail.com>
2023-05-02 16:59:43 +0300

    Veriyi Alma islemi

--- CurrencyConverterApp/CurrencyConverterApp/ViewController.swift
+++ CurrencyConverterApp/CurrencyConverterApp/ViewController.swift
@@ -26,6 +26,50 @@ class ViewController: UIViewController {
     }
     
     @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // 1. Request - Session = web adresine gitmek istek yollamak
+        // 2. Response - Data = gittigimiz yerde veriyi almak
+        // 3. Parsing - JSON Serialization = Aldigimiz Verileri islemek
+        
+        // MARK: 1.Adim
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
+        let session = URLSession.shared
+        //Closure
+        let task = session.dataTask(with: url!) { (data,response ,error ) in
+            
+            if error != nil{
+                
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+            } else {
+                //2.Adim
+                if data != nil{
+                    
+                    do {
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data! , options: JSONSerialization.ReadingOptions.mutableContainers)
+                        
+                        //ASYNC
+                        DispatchQueue.main.async {
+                            print(jsonResponse)
+                        }
+                        
+                    } catch  {
+                        print(error.localizedDescription)
+                    }
+                    
+                    
+                }
+                
+                
+            }
+            
+            
+        }
+        task.resume()
+        
+        
     }
     
 
