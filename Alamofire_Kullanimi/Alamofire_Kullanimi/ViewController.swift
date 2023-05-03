81ee0f3fa6b2f93f8fa4228d6a564761af437e9e
tahaturan <tahaaturann16@gmail.com>
2023-05-03 11:14:42 +0300

    Alamofire kullanimi initial commit

---
+++ Alamofire_Kullanimi/Alamofire_Kullanimi/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Alamofire_Kullanimi
+//
+//  Created by Taha Turan on 3.05.2023.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        
+    }
+    
+
+
+
+}
+
