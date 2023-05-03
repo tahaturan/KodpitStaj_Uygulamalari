bbc9e4bbf50a3316f86190157cb601c9b7eb5fc5
tahaturan <tahaaturann16@gmail.com>
2023-05-03 11:15:15 +0300

    Veri ekleme

--- Alamofire_Kullanimi/Alamofire_Kullanimi/ViewController.swift
+++ Alamofire_Kullanimi/Alamofire_Kullanimi/ViewController.swift
@@ -13,11 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+       urunEkle()
         
     }
     
-
+    func urunEkle()  {
+        let param:Parameters = ["title":"Deneme"]
+        AF.request("https://dummyjson.com/products/add" , method: .post , parameters: param).response {
+            response in
+            
+            if let data = response.data {
+                
+                do {
+                    if let json =  try JSONSerialization.jsonObject(with: data) as? Dictionary<String,Any> {
+                        print(json)
+                    }
+                } catch  {
+                    print(error.localizedDescription)
+                }
+                
+                
+            }
+            
+            
+        }
+    }
 
 
 }
