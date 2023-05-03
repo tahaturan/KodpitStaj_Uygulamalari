1d0701b9e6b017a3362d0e8a71fa1d1d8c333c02
tahaturan <tahaaturann16@gmail.com>
2023-05-03 14:23:28 +0300

    Veri ekleme islemi Firebase

--- Firebase_Kullanimi/Firebase_Kullanimi/ViewController.swift
+++ Firebase_Kullanimi/Firebase_Kullanimi/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var ref:DatabaseReference?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        kisiEkle()
+    }
+    
+    
+    func kisiEkle()  {
+        let yeniKisi = Kisiler(kisiAd: "Berk", kisiYas: 15)
+        
+        let dict:[String:Any] = ["kisiAd":yeniKisi.kisiAd! , "kisiYas":yeniKisi.kisiYas!]
+        
+        let newRef = ref?.child("kisiler").childByAutoId()
+        
+        newRef?.setValue(dict)
     }
 
 
