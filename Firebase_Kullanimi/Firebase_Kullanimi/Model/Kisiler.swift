32c138aa5bcbf34dcc0326ab4fa871cc74920cf6
tahaturan <tahaaturann16@gmail.com>
2023-05-03 14:07:15 +0300

    Firebase initial commit

---
+++ Firebase_Kullanimi/Firebase_Kullanimi/Model/Kisiler.swift
@@ -0,0 +1,19 @@
+//
+//  Kisiler.swift
+//  Firebase_Kullanimi
+//
+//  Created by Taha Turan on 3.05.2023.
+//
+
+import Foundation
+
+class Kisiler {
+    let kisiAd:String?
+    let kisiYas:Int?
+    
+    init(kisiAd: String, kisiYas: Int) {
+        self.kisiAd = kisiAd
+        self.kisiYas = kisiYas
+    }
+}
+
---
+++ Firebase_Kullanimi/Firebase_Kullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase_Kullanimi
+//
+//  Created by Taha Turan on 3.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
