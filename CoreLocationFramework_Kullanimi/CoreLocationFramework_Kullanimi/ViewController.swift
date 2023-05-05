b6ce7c96dc1fd3c407cca0245d8ef1a44eebef83
tahaturan <tahaaturann16@gmail.com>
2023-05-05 13:48:00 +0300

    Core Location Kullanimi

--- CoreLocationFramework_Kullanimi/CoreLocationFramework_Kullanimi/ViewController.swift
+++ CoreLocationFramework_Kullanimi/CoreLocationFramework_Kullanimi/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    var locationManager : CLLocationManager = CLLocationManager()
+    
+    @IBOutlet weak var enlemLabel: UILabel!
+    @IBOutlet weak var boylamLabel: UILabel!
+    @IBOutlet weak var hizLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.startUpdatingLocation()
+        
+        
     }
 
 
 }
 
+
+extension ViewController:CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let sonKonum:CLLocation = locations[locations.count - 1]
+        
+        enlemLabel.text = "Enlem: \(sonKonum.coordinate.latitude)"
+        boylamLabel.text = "Boylam: \(sonKonum.coordinate.longitude)"
+        hizLabel.text = "Hiz: \(sonKonum.speed)"
+        
+    }
+    
+}
