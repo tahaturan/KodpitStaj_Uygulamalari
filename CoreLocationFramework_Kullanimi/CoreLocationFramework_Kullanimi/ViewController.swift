c487270ef90ed245c7819fade7e952842c7f8156
tahaturan <tahaaturann16@gmail.com>
2023-05-05 13:30:55 +0300

    initial commit

---
+++ CoreLocationFramework_Kullanimi/CoreLocationFramework_Kullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocationFramework_Kullanimi
+//
+//  Created by Taha Turan on 5.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
