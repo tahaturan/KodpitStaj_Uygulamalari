1b2d71740bd034822f9be5c442e808f2a07e6394
tahaturan <tahaaturann16@gmail.com>
2023-05-08 10:51:12 +0300

    Haritaya Pin Ekleme

--- SeyahatKitabim/SeyahatKitabim/ViewController.swift
+++ SeyahatKitabim/SeyahatKitabim/ViewController.swift
@@ -24,12 +24,37 @@ class ViewController: UIViewController, MKMapViewDelegate  , CLLocationManagerDe
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
     }
+    
+    
+   @objc func chooseLocation(gestureRecognizer:UILongPressGestureRecognizer)  {
+       
+       if gestureRecognizer.state == .began {
+           let touchPoint = gestureRecognizer.location(in: self.mapView)
+           let touchCoordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+           
+           let annotation = MKPointAnnotation()
+           annotation.coordinate = touchCoordinates
+           annotation.title = "New Annotation"
+           annotation.subtitle = "Travel Book"
+           self.mapView.addAnnotation(annotation)
+       }
+       
+       
+       
+    }
+    
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location  = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
         
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
         
         let region = MKCoordinateRegion(center: location, span: span)
         
