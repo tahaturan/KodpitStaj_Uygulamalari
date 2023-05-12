49de0878ff2d82d42fbb849dd8235e2fec2a306f
tahaturan <tahaaturann16@gmail.com>
2023-05-12 16:37:25 +0300

    View Fonksiyonlari

--- CryptoAppViper/CryptoAppViper/View.swift
+++ CryptoAppViper/CryptoAppViper/View.swift
@@ -13,9 +13,82 @@ import UIKit
 // ViewController
 
 protocol AnyView {
+    var presenter:AnyPresenter? {get set}
     
+    func update(with cryptos:[Crypto])
+    func update(with error:String)
 }
 
 class CryptoViewController: UIViewController , AnyView {
+    var presenter: AnyPresenter?
+    
+    var cryptos = [Crypto]()
+    
+    //tableView
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    //message label
+    private let messageLabel : UILabel = {
+       let label = UILabel()
+        label.isHidden = false
+        label.text = "Downloading..."
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        view.addSubview(messageLabel)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    
+        messageLabel.frame = CGRect(x:view.frame.width / 2 - 100 , y: view.frame.height / 2 - 25, width: 200, height: 50)
+    }
+    
+    func update(with cryptos: [Crypto]) {
+        DispatchQueue.main.async {
+            self.cryptos = cryptos
+            self.messageLabel.isHidden = true
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+    
+    func update(with error: String) {
+        <#code#>
+    }
+
+}
+
+//MARK: - TableView islemleri
+extension CryptoViewController : UITableViewDelegate , UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.cryptos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+    
     
 }
+//MARK: -TableView Extension sonu
+
