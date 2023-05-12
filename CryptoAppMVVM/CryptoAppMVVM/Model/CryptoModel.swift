d4ff498ddfe1c5fa329ba2f39de67e7979e6e5e6
tahaturan <tahaaturann16@gmail.com>
2023-05-12 11:42:10 +0300

    klasorleme ve Model olusturma

---
+++ CryptoAppMVVM/CryptoAppMVVM/Model/CryptoModel.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoModel.swift
+//  CryptoAppMVVM
+//
+//  Created by Taha Turan on 12.05.2023.
+//
+
+import Foundation
+
+struct CryptoModel : Decodable{
+    let currency : String
+    let price : String
+}
